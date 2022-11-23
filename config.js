4dad56a098da5891849c54e7283b051a1bd56159
tony <68637378+tonymaa@users.noreply.github.com>
2022-11-23 13:10:00 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,80 @@
+navs = [
+	{
+	  navName: "个人主页导航",
+	  items: [
+		{
+		  title: "github",
+		  describe: "tony 的git hub主页",
+		  url: "https://github.com/tonymaa",
+		  img: "img/github.png",
+		  buttons: [
+			{
+			  text: "github 主页",
+			  url: "https://github.com/tonymaa"
+			}
+		  ]
+		},
+		{
+		  title: "bilibili",
+		  describe: "tony 的 bilibili",
+		  url: "https://space.bilibili.com/227369250",
+		  img: "img/bilibili.png",
+		  buttons: [
+			{
+			  text: "bilibili 主页",
+			  url: "https://space.bilibili.com/227369250"
+			}
+		  ]
+		},
+		{
+		  title: "tony blog",
+		  describe: "tony 的个人博客",
+		  url: "http://124.220.134.62/",
+		  img: "img/avatar.jpg",
+		  buttons: [
+			{
+			  text: "博客主页",
+			  url: "http://124.220.134.62/"
+			},
+			{
+			  text: "管理",
+			  url: "http://124.220.134.62/admin"
+			}
+		  ]
+		},
+		{
+		  title: "tony 的公开相册",
+		  describe: "tony 的公开相册",
+		  url: "http://124.220.134.62/pictures.html?page=pictures",
+		  img: "img/tonyalbum.png",
+		  buttons: [
+			{
+			  text: "相册",
+			  url: "http://124.220.134.62/pictures.html?page=pictures"
+			}
+		  ]
+		}
+	  ]
+	},
+	{
+	  navName: "向日葵",
+	  items: [
+		{
+		  title: "company computer",
+		  describe: "company computer",
+		  url: "https://sunlogin.oray.com/share?shareid=1002195235",
+		  img: "img/xrk_logo.png",
+		  buttons: [
+			{
+			  text: "打开主机",
+			  url: "https://sunlogin.oray.com/share?shareid=1002195235"
+			},
+			{
+			  text: "管理",
+			  url: "https://console.sunlogin.oray.com/"
+			}
+		  ]
+		}
+	  ]
+	}
+];
