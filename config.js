3b076d69b1074b05fea7e891cd74a3c93bb89d2b
tony <1605337475@qq.com>
2024-04-07 23:52:32 +0800

    hw75

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ navs = [
 	  navName: "个人主页导航",
 	  items: [
 		{
-		  title: "github",
-		  describe: "tony 的git hub主页",
+		  title: "Github",
+		  describe: "Tony 的 Github主页",
 		  url: "https://github.com/tonymaa",
 		  img: "img/github.png",
 		  buttons: [
 			{
-			  text: "github 主页",
+			  text: "Github 主页",
 			  url: "https://github.com/tonymaa",
 			  name: "github",
 			}
@@ -17,7 +17,7 @@ navs = [
 		},
 		{
 		  title: "bilibili",
-		  describe: "tony 的 bilibili",
+		  describe: "Tony 的 bilibili",
 		  url: "https://space.bilibili.com/227369250",
 		  img: "img/bilibili.png",
 		  buttons: [
@@ -29,8 +29,8 @@ navs = [
 		  ]
 		},
 		{
-		  title: "tony 的公开相册",
-		  describe: "tony 的公开相册",
+		  title: "公开相册",
+		  describe: "Tony 的公开相册",
 		  url: "http://h1406y6769.imdo.co:47891/#/album",
 		  img: "img/tonyalbum.png",
 		  buttons: [
@@ -42,5 +42,23 @@ navs = [
 		  ]
 		}
 	  ]
+	},
+	{
+		navName: "工具",
+		items: [
+			{
+				title: "瀚文75",
+				describe: "瀚文75键盘管理界面",
+				url: "./zmkx.app/index.html",
+				img: "https://img.zcool.cn/community/01i76galyvo4os72t7jmaq3939.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100",
+				buttons: [
+					{
+						text: "管理界面",
+						url: "./zmkx.app/index.html",
+						name: "hw75",
+					}
+				]
+			},
+		]
 	}
 ];
